2f8fc866eb3b86c263eb00aeff40ca6e35113bf7
Aleksandr <zgyrski202@gmail.com>
2026-02-22 12:54:06 +0200

    Replacement of if by switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,33 +11,34 @@ function transformStateWithClones(state, actions) {
   const copyState = { ...state };
 
   for (let index = 0; index < actions.length; index++) {
-    if (actions[index].type === 'addProperties') {
-      const cop = { ...Object.assign(copyState, actions[index].extraData) };
+    let copy = {};
 
-      history.push(cop);
+    switch (actions[index].type) {
+      case 'addProperties':
+        copy = { ...Object.assign(copyState, actions[index].extraData) };
+        break;
+
+      case 'removeProperties':
+        copy = { ...copyState };
+
+        for (let num = 0; num < actions[index].keysToRemove.length; num++) {
+          delete copyState[actions[index].keysToRemove[num]];
+          copy = { ...copyState };
+        }
+        break;
+
+      case 'clear':
+        for (const key in copyState) {
+          delete copyState[key];
+          copy = { ...copyState };
+        }
+        break;
+
+      default:
+        copy = { ...copyState };
     }
 
-    if (actions[index].type === 'removeProperties') {
-      let cop = { ...copyState };
-
-      for (let num = 0; num < actions[index].keysToRemove.length; num++) {
-        delete copyState[actions[index].keysToRemove[num]];
-        cop = { ...copyState };
-      }
-
-      history.push(cop);
-    }
-
-    if (actions[index].type === 'clear') {
-      let cop = {};
-
-      for (const key in copyState) {
-        delete copyState[key];
-        cop = { ...copyState };
-      }
-
-      history.push(cop);
-    }
+    history.push(copy);
   }
 
   return history;
